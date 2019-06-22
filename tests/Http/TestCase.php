e4d030a2c996e67a8ae2003609be516bbaeae868
Thomas Flori <thflori@gmail.com>
2019-06-23 00:58:20 +0200

    add Http\TestCase::post() method for testing post requests

--- tests/Http/TestCase.php
+++ tests/Http/TestCase.php
@@ -4,6 +4,7 @@ namespace Test\Http;
 
 use App\Http\HttpKernel;
 use App\Model\Request;
+use function GuzzleHttp\Psr7\stream_for;
 use Tal\Psr7Extended\ServerResponseInterface;
 
 abstract class TestCase extends \Test\TestCase
@@ -15,6 +16,27 @@ abstract class TestCase extends \Test\TestCase
 
         foreach ($headers as $header => $value) {
             $request = $request->withHeader($header, $value);
+        }
+
+        $kernel = new HttpKernel($this->app);
+        return $this->app->run($kernel, $request);
+    }
+
+    protected function post(string $uri, array $query = [], $body = null, array $headers = []): ServerResponseInterface
+    {
+        $request = (new Request('post', $uri))
+            ->withQueryParams($query);
+
+        foreach ($headers as $header => $value) {
+            $request = $request->withHeader($header, $value);
+        }
+
+        if (is_array($body)) {
+            $request = $request->withParsedBody($body);
+        } elseif (is_string($body)) {
+            $request = $request->withBody(stream_for($body));
+        } else {
+            $request = $request->withBody($body);
         }
 
         $kernel = new HttpKernel($this->app);
