6b443be8bc2ad326fd6c568ea1ba7f243c73660f
Thomas Flori <thflori@gmail.com>
2019-11-09 16:56:01 +0100

    add a basic footer

--- resources/layouts/fullPage.php
+++ resources/layouts/fullPage.php
@@ -200,42 +200,7 @@ use App\Application;
         </div>
       </main>
 
-      <!-- Footer -->
-      <footer class="page-footer teal darken-1">
-        <div class="container">
-          <div class="row">
-            <div class="col l7 s12">
-              <h5 class="white-text">Footer Content</h5>
-              <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              <p class="grey-text text-lighten-4">
-                This site is protected by reCAPTCHA and the Google
-                <a href="https://policies.google.com/privacy">Privacy Policy</a> and
-                <a href="https://policies.google.com/terms">Terms of Service</a> apply.
-              </p>
-            </div>
-            <div class="col l4 offset-l1 s12">
-              <h5 class="white-text">Links</h5>
-              <ul>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="footer-copyright">
-          <div class="container">
-            <div class="row">
-              <div class="col s12">
-                © 2018 Copyright Thomas Flori
-                <a class="grey-text text-lighten-4 right" href="https://github.com/tflori/riki-community">View on
-                  <b>GitHub</b></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <?= $v->fetch('partials/footer') ?>
 
     </div>
 
---
+++ resources/views/partials/footer.php
@@ -0,0 +1,41 @@
+<footer class="page-footer teal darken-1">
+  <div class="container">
+    <div class="row">
+      <div class="col l8 s12">
+        <h5 class="white-text">Ríki Community</h5>
+        <p class="grey-text text-lighten-4">The ríki community project is an open source project to introduce the ríki framework and the development philosophies of frameworkless development.</p>
+        <p class="grey-text text-lighten-4">
+          This site is protected by reCAPTCHA and the Google
+          <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+          <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+        </p>
+      </div>
+      <div class="col l4 s12">
+        <div class="row">
+          <div class="col m6 s12">
+            <h5 class="white-text">Favorite Libraries</h5>
+            <a class="fav-lib" href="https://packagist.org/packages/tflori/orm">tflori/orm</a>
+            <a class="fav-lib" href="https://packagist.org/packages/tflori/syna">tflori/syna</a>
+            <a class="fav-lib" href="https://packagist.org/packages/tflori/verja">tflori/verja</a>
+            <a class="fav-lib" href="https://packagist.org/packages/tflori/breyta">tflori/breyta</a>
+            <a class="fav-lib" href="https://packagist.org/packages/tflori/dependency-injector">tflori/dependency-injector</a>
+          </div>
+          <div class="col m6 s12">
+            <h5 class="white-text">Links</h5>
+            <a class="fav-lib" href="/imprint">Imprint</a>
+            <a class="fav-lib" href="/about">About Us</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          © <?= date('Y'); ?> Copyright Thomas Flori <a class="grey-text text-lighten-4 right" href="https://github.com/tflori/riki-community">View on <b>GitHub</b></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
