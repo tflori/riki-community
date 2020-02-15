9f9d40fc9cebe1d778d3c772da71574268337ddc
Thomas Flori <t.flori@sportradar.com>
2020-02-15 21:39:33 +0100

    fix debug shell script

--- docker/php/debug.sh
+++ docker/php/debug.sh
@@ -3,10 +3,10 @@
 export XDEBUG_CONFIG="idekey=ANY_IDE remote_host=`route -nA inet|egrep ^0.0.0.0|tr -s ' '|cut -d' ' -f2`"
 export PHP_IDE_CONFIG="serverName=community"
 
-if [ -f $1 ]; then
-  exec php "$@"
-elif which $1 > /dev/null 2>&1; then
-  file=$(which $1)
+if [ ! -f "$1" ] && which "$1" > /dev/null 2>&1; then
+  file=$(which "$1")
   shift
-  exec php -dzend_extension=xdebug.so $file "$@"
+  set -- "$file" "$@"
 fi
+
+exec php -dzend_extension=xdebug.so "$@"
