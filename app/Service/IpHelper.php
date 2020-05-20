1321fff02407edb5ece2649e423222e8786266a8
Thomas Flori <thflori@gmail.com>
2020-05-20 08:01:27 +0200

    improve readability and add documentation for IpHelper

--- app/Service/IpHelper.php
+++ app/Service/IpHelper.php
@@ -4,7 +4,20 @@ namespace App\Service;
 
 class IpHelper
 {
-    public function isInRange(string $ip, string $range)
+    /**
+     * Check if the $ip is in $range
+     *
+     * Accepted range definitions:
+     * - an ip address (e. g. 192.168.0.1)
+     * - an ipv4 range (e. g. 10.23.0.0/16)
+     * - an ipv6 range (e. g. fe80::/64)
+     * - a regular expression (e. g. 192.168.0.\d+)
+     *
+     * @param string $ip
+     * @param string $range
+     * @return bool
+     */
+    public function isInRange(string $ip, string $range): bool
     {
         if (strpos($range, '/') === false) {
             return (bool)preg_match('/' . $range . '/', $ip);
@@ -20,14 +33,24 @@ class IpHelper
         return $ipNetBits === $netBits;
     }
 
-    protected function iptToBits($ip)
+    /**
+     * Get a string of bits from ip address
+     *
+     * Works for ipv4 and ipv6 addresses
+     *
+     * @param $ip
+     * @return string
+     */
+    protected function iptToBits($ip): string
     {
         $inet = inet_pton($ip);
         $chars = str_split($inet);
-        $binaryip = '';
+        $binaryIp = '';
         foreach ($chars as $char) {
-            $binaryip .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT);
+            $binaryChar = decbin(ord($char));
+            // fill left with 0es
+            $binaryIp .= str_pad($binaryChar, 8, '0', STR_PAD_LEFT);
         }
-        return $binaryip;
+        return $binaryIp;
     }
 }
