cc4ee32f13321de378ff6569ef6f074b135532d7
Thomas Flori <thflori@gmail.com>
2020-10-10 15:13:54 +0200

    remove check for users without email or display name
    It was anyway just showing an information and is already applied...
    
    It should have been just showing the information that you have to
    remove the users or set the email and display name. The migrations
    have to be non-interactive.

--- resources/database/migrations/2020-05-07T22.56.57Z_AlterUsersTable.php
+++ resources/database/migrations/2020-05-07T22.56.57Z_AlterUsersTable.php
@@ -2,17 +2,12 @@
 
 namespace Breyta\Migration;
 
-use App\Application;
 use Breyta\AbstractMigration;
-use Community\Model\User;
-use Hugga\Input\Question\Confirmation;
 
 class AlterUsersTable extends AbstractMigration
 {
     public function up(): void
     {
-        $this->checkForEmptyUsers();
-
         $this->exec('ALTER TABLE users
             ALTER COLUMN email SET NOT NULL,
             ALTER COLUMN display_name SET NOT NULL');
@@ -23,23 +18,5 @@ class AlterUsersTable extends AbstractMigration
         $this->exec('ALTER TABLE users
             ALTER COLUMN email DROP NOT NULL,
             ALTER COLUMN display_name DROP NOT NULL');
-    }
-
-    private function checkForEmptyUsers()
-    {
-        $count = User::query()->where('displayName IS NULL')->orWhere('email IS NULL')->count();
-        if ($count > 0) {
-            $console = Application::console();
-            if ($console->ask(new Confirmation(
-                'There are ' . $count . ' users without displayName or email. ${red}Remove them?${r}' . PHP_EOL,
-                false
-            ))) {
-                $console->line('ok we remove them!');
-            }
-        } else {
-            $console = Application::console();
-            $console->line('no users without dn or email');
-        }
-        //$this->exec('DELETE FROM users WHERE display_name IS NULL OR email IS NULL');
     }
 }
